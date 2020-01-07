87d31dc22d35e044607523001c688eaece9ad036
Alvaro <alvarorameau@gmail.com>
2020-01-07 13:12:04 -0300

    feet to screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import WisperIframe from './iframe/WhisperIframe';
 
+
+import fit_ui_container from './fit_to_screen';
+
 function App() {
+
+  useEffect(() => {
+    fit_ui_container();
+
+    window.onresize = fit_ui_container;
+  })
+
   return (
     <WisperIframe src="http://localhost:3000" />
   );
